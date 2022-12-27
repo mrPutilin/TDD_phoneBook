2ce1f11bea3aab056edee5992a6a7e56b7a27c6c
Evgeniy Putilin <mr.putilinz@gmail.com>
2022-12-27 16:46:26 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> book;
+
+    public PhoneBook() {
+        book = new HashMap<>();
+    }
+
+    public boolean add(String name, String phone) {
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,48 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 public class PhoneBookTest {
+
+    static PhoneBook sut;
+
+    @BeforeAll
+    public static void mes () {
+        System.out.println("TESTS STARTED");
+        sut = new PhoneBook();
+    }
+
+    @BeforeEach
+    public void init() {
+        System.out.println("Test started");
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void shouldReturnAmountContacts(String name, String phone, int expected) {
+
+        Assertions.assertEquals(expected, sut.add(name, phone));
+
+    }
+
+    private static Stream<Arguments> source() {
+        return Stream.of(Arguments.of("Tony", "444", 1 ),
+                        Arguments.of("Ben", "555", 2),
+                        Arguments.of("Tony", "666", 2));
+    }
+
+    @AfterEach
+    public void finishedEach() {
+        System.out.println("Test completed");
+    }
+
+    @AfterAll
+    public static void finishedAll () {
+        System.out.println("TESTS FINISHED");
+    }
+
+
 }
